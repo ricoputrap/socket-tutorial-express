89fcb1adaccf64a721d63a169846389d55461d42
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-09-10 23:28:13 +0700

    Implement basic broadcasting message to ANOTHER client

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 
@@ -17,7 +16,11 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("===== User Connected: " + socket.id);
+  console.log("===== Connected:", socket.id)
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+  })
 
   socket.on("disconnect", () => {
     console.log("===== Client disconnected.")
