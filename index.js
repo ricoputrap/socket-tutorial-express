068a3867f8c1b003a466318d1c0ccde581f02a35
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-09-10 23:15:25 +0700

    Setup a socket io server

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
-const app = express();
 const PORT = 3001;
+const app = express();
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log("SERVER IS RUNNING ON PORT: ", PORT);
+const server = app.listen(PORT);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5173/",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("===== User Connected: " + socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("===== Client disconnected.")
+  });
 });
